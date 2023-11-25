ec3836ee0f00e259275fcb8cbbeb27ccf07cd604
Paul Gribble <pgribble@uwo.ca>
2023-11-25 15:51:50 -0500

    misc

--- simple.py
+++ simple.py
@@ -85,7 +85,7 @@ def go(model_name, jw, n_batch=20000, batch_size=256):
     plot_stuff(data, model_name + "/" + model_name, batch=batch)
 
 if __name__ == "__main__":
-    jerk_weights = [0,0,100,100,200,200,400,400,600,600]
+    jerk_weights = [0,0,200,200,400,400,800,800]
     model_name = "jerk_"
     n_batch = int(sys.argv[1])
     batch_size = int(sys.argv[2])
