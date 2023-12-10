9ef6ed7fb7b248be0dcdb4e1ebac0a86e451305e
Paul Gribble <pgribble@uwo.ca>
2023-12-10 14:28:50 -0500

    misc

--- simple_utils.py
+++ simple_utils.py
@@ -196,7 +196,12 @@ def cal_loss(data, params=None, dt=0.01):
 
 
 #    loss_weights = np.array([1e+0, 1e-4, 1e-5, 3e-5, 2e-2, 2e+2])
-    loss_weights = np.array([1e+0, 1e-4, 1e-5, 3e-5, 1e-10, 0.0])
+    loss_weights = np.array([1e+0,   # position
+                             1e-4,   # muscle
+                             1e-5,   # muscle_derivative
+                             3e-5,   # hidden
+                             1e-12,  # hidden_derivative
+                             0e+2])  # jerk
 
     if (not params==None):
         loss_weights[5] = params['jw']
